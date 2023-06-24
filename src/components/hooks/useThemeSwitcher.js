dd63200f9344f37238bbdf47450d04fdc1aa2aac
Devi7uke <dvlk8003@gmail.com>
2023-06-24 16:34:45 -0600

    Third Commit

--- src/components/hooks/useThemeSwitcher.js
+++ src/components/hooks/useThemeSwitcher.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 
 const useThemeSwitcher = () => {
-  const preferDarkQuery = "(prefer-color-scheme: dark)"
+  
+  const preferDarkQuery = "(prefers-color-scheme: dark)"
   const [mode, setMode] = useState("")
 
   useEffect(() => {
@@ -43,7 +44,8 @@ const useThemeSwitcher = () => {
     if(mode === "dark"){
       window.localStorage.setItem("theme", "dark")
       document.documentElement.classList.add("dark")
-    }else{
+    }
+    if(mode === "light"){
       window.localStorage.setItem("theme", "light")
       document.documentElement.classList.remove("dark")
     }
